eca48e57b95f8623d73b2bbca80a5a40a179e844
Vinny <Vinny>
2019-06-23 21:00:50 +0300

    23/6/2019
    Java update - Rest API

--- Java/CarDemo/src/main/java/com/example/CarDemo/CarDemoApplication.java
+++ Java/CarDemo/src/main/java/com/example/CarDemo/CarDemoApplication.java
@@ -1,5 +1,8 @@
 package com.example.CarDemo;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,10 +25,11 @@ public class CarDemoApplication {
   @Autowired 
   private CarRepository repositoryCar;
   
-@Autowired TripRepository tRepo;
-  
   @Autowired 
   private OwnerRepository repositoryOwner;
+  
+  @Autowired 
+  private TripRepository repositoryTrip;
 
   private static final Logger logger = LoggerFactory.getLogger(CarDemoApplication.class);
 
@@ -36,8 +40,10 @@ public class CarDemoApplication {
     
     logger.info("Hello Class");
   }
-
   
+  
+  /* @Bean is used to simulate user input into command line interface.
+     In this case we use it to input data into the tables we defined */
   @Bean
   CommandLineRunner runner(){
     return args -> {
@@ -46,17 +52,33 @@ public class CarDemoApplication {
     	Owner owner2 = new Owner("Mary", "Smith");
       /*persist (save) the owners info into the DB using the Owners we already created. 
         Note, these owners have no cars defined since we used the constructor without cars.
-        defining multiple constructors with the same name and different composition is alowed
+        defining multiple constructors with the same name and different composition is allowed
         and called 'Overloading'*/
     	repositoryOwner.save(owner1);
     	repositoryOwner.save(owner2);
+    	
+    	//Add trip objects
+        Trip trip1 = new Trip("Trip A");
+        Trip trip2 = new Trip("Trip B");
+        repositoryTrip.save(trip1);
+        repositoryTrip.save(trip2);
+
+        // Add Sets of trips so we can add them to the Car.
+        Set<Trip> trips1= new HashSet<Trip>();
+        trips1.add(trip1);
+        trips1.add(trip2);
+        
+        Set<Trip> trips2= new HashSet<Trip>();
+        trips2.add(trip1);
+        
+        Set<Trip> trips3= new HashSet<Trip>();
+        trips3.add(trip2);
+
       /*Save demo data for cars into database using a command line runner that behaves as if it was a user entering data 
     	using 'save' that is defined in CrudRepository*/
-    	repositoryCar.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 59000, owner1, null));
-    	repositoryCar.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2014, 29000, owner2, null));    
-    	repositoryCar.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000, owner1, null));
-    	
-    	tRepo.save(new Trip("TripToTheMoon"));
+    	repositoryCar.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 59000, owner1, trips1));
+    	repositoryCar.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2014, 29000, owner2, trips2));    
+    	repositoryCar.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000, owner1, trips3));
     };
   } 
 
--- Java/CarDemo/src/main/java/com/example/CarDemo/da/TripRepository.java
+++ Java/CarDemo/src/main/java/com/example/CarDemo/da/TripRepository.java
@@ -1,9 +1,8 @@
 package com.example.CarDemo.da;
-
 import org.springframework.data.repository.CrudRepository;
-
 import com.example.CarDemo.domain.Trip;
 
 public interface TripRepository extends CrudRepository<Trip, Long> {
 
 }
+
