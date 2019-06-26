06fcd9e4341af0ae04447d297976962e0f0ca91b
v <v>
2019-06-26 20:39:40 +0300

    Java backend + angular connection
    26/6/19

--- Java/CarDemo/src/main/java/com/example/CarDemo/domain/Car.java
+++ Java/CarDemo/src/main/java/com/example/CarDemo/domain/Car.java
@@ -1,16 +1,17 @@
 package com.example.CarDemo.domain;
 
-	import java.util.Set;
-	import javax.persistence.CascadeType;
-	import javax.persistence.Entity;
-	import javax.persistence.FetchType;
-	import javax.persistence.GeneratedValue;
-	import javax.persistence.GenerationType;
-	import javax.persistence.Id;
-	import javax.persistence.JoinColumn;
-	import javax.persistence.JoinTable;
-	import javax.persistence.ManyToMany;
-	import javax.persistence.ManyToOne;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Car {
